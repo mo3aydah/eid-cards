6ce76831da9adeed08d606e81c4aa33b1d83e65b
Atheer F. Alotaibi <84769445+atheer0998@users.noreply.github.com>
2025-06-02 09:46:05 +0300

    -

--- assets/js/zamil.js
+++ assets/js/zamil.js
@@ -33,7 +33,7 @@ function DownloadCanvasAsImage() {
 }
 
 
-imageObj.src = "assets/images/alzamil.png"; 
+imageObj.src = "assets/images/zamil.png"; 
 
 var downloadCardButton = document.getElementById('downloadCard');
 downloadCardButton.addEventListener('click', function(e){
