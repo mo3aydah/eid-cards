4091ef2514d25a2b9c51fc1cd499f2e86fc301c0
Atheer F. Alotaibi <84769445+atheer0998@users.noreply.github.com>
2025-06-02 16:05:42 +0300

    updating the img

--- assets/js/plant.js
+++ assets/js/plant.js
@@ -33,7 +33,7 @@ function DownloadCanvasAsImage() {
 }
 
 
-imageObj.src = "assets/images/plant.png"; 
+imageObj.src = "assets/images/plant.jpg"; 
 
 var downloadCardButton = document.getElementById('downloadCard');
 downloadCardButton.addEventListener('click', function(e){
